2109d22e65a98b399f23ee071426c115beec786c
Darielescobar <escobardariel5@gmail.com>
2026-03-23 22:39:43 +0000

    las variables han sido renombradas

--- src/app.js
+++ src/app.js
@@ -8,27 +8,27 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
+let pronouns = ['the', 'our'];
+let adjectives = ['great', 'big'];
+let nouns = ['jogger', 'racoon'];
 let extensions = ['.com', '.net', '.us', '.io'];
 
 
-for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
-    for (let k = 0; k < noun.length; k++) {
+for (let i = 0; i < pronouns.length; i++) {
+  for (let j = 0; j < adjectives.length; j++) {
+    for (let k = 0; k < nouns.length; k++) {
       for (let l = 0; l < extensions.length; l++) {
-        console.log(pronoun[i] + adj[j] + noun[k] + extensions[l]);
+        console.log(pronouns[i] + adjectives[j] + nouns[k] + extensions[l]);
       }
     }
   }
 }
 
 
-for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
-    for (let k = 0; k < noun.length; k++) {
-      let word = pronoun[i] + adj[j] + noun[k];
+for (let i = 0; i < pronouns.length; i++) {
+  for (let j = 0; j < adjectives.length; j++) {
+    for (let k = 0; k < nouns.length; k++) {
+      let word = pronouns[i] + adjectives[j] + nouns[k];
 
       
       if (word.endsWith("es")) {
